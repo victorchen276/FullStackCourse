bd503d8fdda5971fce25e213f33c2d400f31528c
Victor Chen <victorchen276@gmail.com>
2020-07-16 23:55:25 +1200

    add project code

---
+++ src/main/java/com/mtzendo/CourseDemo/CourseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mtzendo.CourseDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtzendo/CourseDemo/CourseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtzendo.CourseDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
